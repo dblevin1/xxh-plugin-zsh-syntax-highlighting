cffbd21d657fccd7a1b4bd64d4f167ae4964f1d6
Daniel Blevins <daniel.blevins@asu.edu>
2023-06-08 16:58:23 -0700

    Add Syntax Highlighting Changes

--- build.sh
+++ build.sh
@@ -21,6 +21,20 @@ do
     cp $CDIR/$f $build_dir/
 done
 
+url='https://github.com/zsh-users/zsh-syntax-highlighting.git'
+home_dir=$build_dir/zsh-syntax-highlighting
+cd $build_dir
+
+[ $QUIET ] && arg_q='-q' || arg_q=''
+[ $QUIET ] && arg_s='-s' || arg_s=''
+[ $QUIET ] && arg_progress='' || arg_progress='--show-progress'
+
+if [ -x "$(command -v git)" ]; then
+  git clone $arg_q --depth=1 $url $home_dir
+else
+  echo You should install git: https://duckduckgo.com/?q=install+git+on+linux
+fi
+
 #portable_url='https://,,,/.tar.gz'
 #tarname=`basename $portable_url`
 #
--- pluginrc.zsh
+++ pluginrc.zsh
@@ -2,3 +2,5 @@
 # Put here your functions, environment variables, aliases and whatever you need.
 
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+source $CURR_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
